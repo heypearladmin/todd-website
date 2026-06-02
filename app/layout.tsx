0cf8dfddcbe4b6d0ded2345e881f580e6d5b38ae
Dale Gatchallan <dale@heypearl.io>
2026-06-02 20:29:09 +0800

    Add Google Analytics (G-KBF8X0KTKW)

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,6 +88,17 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','933797919253133');fbq('track','PageView');`,
           }}
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KBF8X0KTKW"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-KBF8X0KTKW');`,
+          }}
+        />
       </body>
     </html>
   );
